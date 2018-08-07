66d712c1daf385c32ceb7e8de28e739f90e7b7dd
user.email <frederico.ferraz@ish.com.br>
2018-08-07 10:29:18 -0300

    Basestrike Parser

---
+++ final_basestrike_parser/basestrike.lua
@@ -0,0 +1,52 @@
+local basestrike = nw.createParser("basestrik", "itentify the usage of base href tag")
+basestrike:setKeys({
+nwlanguagekey.create("basehref.tag"),
+nwlanguagekey.create("basehref.value"),
+nwlanguagekey.create("basehref.full"),
+})
+
+
+function basestrike:basexist(token, first, last)
+local payload = nw.getPayload( 1, 50000 )
+local endmtoken = payload:find("=", last - 2, last + 4)
+local bvalue = payload:find(">", endmtoken + 2, endmtoken +100)
+
+if endmtoken then
+nw.logInfo("primeira funcao rodou")
+basehref = payload:tostring(endmtoken + 2, bvalue -2)
+
+if basehref then
+nw.createMeta(self.keys["basehref.tag"], "exists")
+nw.createMeta(self.keys["basehref.value"], basehref)
+nw.logInfo("final da primeira funcao")
+end
+end       
+end
+
+
+function basestrike:hrefexist(token, first, last)
+nw.logInfo("encontrei o token")
+local payload = nw.getPayload( 1, 50000 )
+local endmtoken = payload:find("href=", last, last + 20)
+local bvalue2 = payload:find(">", endmtoken + 6 , endmtoken + 100)
+
+nw.logInfo("valor do endmtoken")
+nw.logInfo(endmtoken)
+if endmtoken then
+
+local hrefull = payload:tostring(endmtoken + 6, bvalue2 -2)
+nw.logInfo(hrefull)
+if basehref and hrefull then
+local fullhref = basehref .. hrefull
+nw.logInfo(fullhref)
+nw.createMeta(self.keys["basehref.full"], fullhref)
+
+
+end
+end       
+end
+
+basestrike:setCallbacks({
+["<base href="] = basestrike.basexist,
+["<a"] = basestrike.hrefexist,
+})
